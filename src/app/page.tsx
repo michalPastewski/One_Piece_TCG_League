5db4bf45f357a8d1d9836930958a8bc70c234dc2
MPastewski <michpastewski@gmail.com>
2025-02-24 07:59:34 +0100

    OPL-01: Initial commit.

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <main className="w-full">
+        <section className="bg-red-100">
+          <h1>OPTCG</h1>
+          <h1>League</h1>
+        </section>
+      </main>
+      <footer className="absolute bottom-0">FOOTER</footer>
+    </>
+  );
+}
