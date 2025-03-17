51f90e33a19c67107a62e02fc2f1bf59a5af0cc7
Michał 'Pasta' Pastewski <60299341+michalPastewski@users.noreply.github.com>
2025-03-17 14:42:46 +0100

    add matches page [OPL-04.4] (#8)
    * OPL-04.4: adjust table - add oferflow-x
    
    * OPL-04.4: add matches view - basic version
    
    - adjust other pages to render with pageWrapper and fix the routing

--- src/app/match-ups/page.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderWrapper } from '@/components/ui/pageWrapper';
-
-export default function MatchUps() {
-  return (
-    <HeaderWrapper label="MATCH-UPS">
-      <p>hello</p>
-    </HeaderWrapper>
-  );
-}
