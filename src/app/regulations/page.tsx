8d238bb15296b44262bdd982a1580ea402e16522
Michał 'Pasta' Pastewski <60299341+michalPastewski@users.noreply.github.com>
2025-03-18 12:33:23 +0100

    OPL-NONE: [ref] delete unusing code/files/directories (#12)

--- src/app/regulations/page.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderWrapper } from '@/components/ui/pageWrapper';
-
-export default function Regulations() {
-  return (
-    <HeaderWrapper label="REGULATIONS">
-      <p>hello</p>
-    </HeaderWrapper>
-  );
-}
