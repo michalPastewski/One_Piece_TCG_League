42ccc3e31f348a2a8585fc57b0bd0613f2a3d6dd
MPastewski <michpastewski@gmail.com>
2025-05-13 14:37:50 +0200

    OPL-05: Add drizzle ORM and PostgreSQL support
    - add docker compose file for PostgreSQL
    - add drizzle ORM configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+export default defineConfig({
+  schema: './src/services//drizzle/schema.ts',
+  out: './src/services/drizzle/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
